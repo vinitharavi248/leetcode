fbf60e7f869d4cb37cf4c127f178233956546b15
vinitharavi248 <vinitharavi2006@gmail.com>
2026-02-02 20:17:51 +0530

    Time: 10 ms (88.74%) | Memory: 45.1 MB (36.26%) - LeetSync

---
+++ 869-similar-string-groups/similar-string-groups.java
@@ -0,0 +1,45 @@
+class Solution {
+    int[] parent;
+    public int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isSimilar(strs[i], strs[j])) {
+                    union(i, j);
+                }
+            }
+        }
+        int groups = 0;
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) groups++;
+        }
+        return groups;
+    }
+    private boolean isSimilar(String a, String b) {
+        int diff = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                diff++;
+                if (diff > 2) return false;
+            }
+        }
+        return diff == 0 || diff == 2;
+    }
+    private int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+    private void union(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px != py) {
+            parent[py] = px;
+        }
+    }
+}
