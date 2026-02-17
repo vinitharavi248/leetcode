e8501f95401ca05203429d7cb54eec06e95783d5
vinitharavi248 <vinitharavi2006@gmail.com>
2026-02-17 20:26:30 +0530

    Time: 15 ms (61.82%) | Memory: 51.5 MB (56.65%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,24 @@
+class Solution 
+{
+    public int findContentChildren(int[] g, int[] s) 
+    {
+      int count =0;
+      Arrays.sort(g);
+      Arrays.sort(s);
+      int i=0,j=0;
+      while(i<g.length && j<s.length)
+      {
+        if(s[j]>=g[i])
+        {
+            count++;
+            i++;
+            j++;
+        }
+        else
+        {
+            j++;
+        }
+      } 
+      return count; 
+    }
+}
