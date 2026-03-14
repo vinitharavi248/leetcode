7d0d2682e9755c3474abe7cfde6e74bfe8ac3958
vinitharavi248 <vinitharavi2006@gmail.com>
2026-03-14 09:36:41 +0530

    Time: 13 ms (36.22%) | Memory: 46.5 MB (22.69%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        int start=0;
+        int end=s.length()-1;
+        while(start<=end){
+            if(s.charAt(start)!=s.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+
+        }
+        return true;
+    }
+}
